959c31fd5025402eb0d8e1ecfddef08f3462f69a
Peter Meehan <meehanp2@tcd.ie>
2018-02-25 00:16:43 +0000

    let's sleep after quits

--- attach_loop.sh
+++ attach_loop.sh
@@ -1,3 +1,3 @@
 export DISPLAY=:10
 
-gnome-terminal --full-screen -e "sh -c 'while true; do docker attach vim; done'"
+gnome-terminal --full-screen -e "sh -c 'while true; do docker attach vim; sleep 1; done'"
