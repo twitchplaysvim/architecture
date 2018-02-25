e1535275ffb8d59f5c0271ea2a498dcd307671ed
Peter Meehan <meehanp2@tcd.ie>
2018-02-25 00:23:36 +0000

    echo the gnome-terminal so I can kill it later

--- attach_loop.sh
+++ attach_loop.sh
@@ -1,3 +1,5 @@
 export DISPLAY=:10
 
-gnome-terminal --full-screen -e "sh -c 'while true; do docker attach vim; sleep 1; done'"
+gnome-terminal --full-screen -e "sh -c 'while true; do docker attach vim; sleep 1; done'" &
+
+echo $!
